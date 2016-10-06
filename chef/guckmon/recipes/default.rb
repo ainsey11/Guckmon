ae2fb6b54ff1244d873aa1dd5ddac9b158b3b212
ainsey11 <robert@ainsey11.com>
2016-10-07 00:06:46 +0100

    Added Standard Server Chef Cookbook

---
+++ chef/guckmon/recipes/default.rb
@@ -0,0 +1,64 @@
+
+# Cookbook Name:: guckmon
+# Recipe:: default
+#
+# Copyright 2016, Ainsey11.com
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "apt-get update" do
+  command "apt-get update -y"
+end
+
+package 'snmpd' do
+  action :install
+end
+
+service 'snmpd' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/etc/snmp/snmpd.conf" do
+  source "snmpd.conf"
+  mode "0644"
+end
+
+service 'snmpd' do
+	action [:restart]
+end
+
+
+package 'htop' do
+	action :install
+end
+
+package 'iftop' do
+        action :install
+end
+package 'iotop' do
+        action :install
+end
+
+package 'vim' do
+        action :install
+end
+
+package 'iperf' do
+        action :install
+end
+
+package 'git-core' do
+        action :install
+end
+
+cookbook_file "/etc/update-motd.d/10-help-text" do
+  source "10-help-text"
+  mode "755"
+end
+
+bash 'updatemotdfile' do
+	user "root"
+	command "chmod +x /etc/update-motd.d/10-help-text"
+end
+
