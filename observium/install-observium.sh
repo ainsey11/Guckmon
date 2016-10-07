7c96524901f08630a1e5154c2af711eaf9fa10db
ainsey11 <robert@ainsey11.com>
2016-10-07 02:54:07 +0100

    spelling fail

--- observium/install-observium.sh
+++ observium/install-observium.sh
@@ -8,7 +8,7 @@ tar zxvf observium-community-latest.tar.gz
 cd observium
 cp /opt/Guckmon/observium/observiumconfig.php /opt/observium/config.php
 cp /opt/Guckmon/observium/observiumcron /etc/cron.d/observium
-cp /opt/Guckmon/observium/000-defult.conf /etc/apache2/sites-available/000-default.conf
+cp /opt/Guckmon/observium/000-default.conf /etc/apache2/sites-available/000-default.conf
 
 phpenmod mcrypt
 a2enmod rewrite
