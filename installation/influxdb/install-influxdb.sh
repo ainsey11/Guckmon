27cd0d005025f666a2fa4228cda33d7c7fe8d70b
ainsey11 <robert@ainsey11.com>
2016-10-07 23:25:27 +0100

    adding influxdb install script

---
+++ installation/influxdb/install-influxdb.sh
@@ -0,0 +1,7 @@
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+apt-get update
+apt-get install influxdb
+service influxdb start
+influxd config > /etc/influxdb/influxdb.generated.conf
