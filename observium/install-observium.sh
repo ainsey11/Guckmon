029b8dd400d764b9140f106cebb0da5f5cf1872a
ainsey11 <robert@ainsey11.com>
2016-10-07 02:56:16 +0100

    adding rm in

--- observium/install-observium.sh
+++ observium/install-observium.sh
@@ -1,7 +1,9 @@
 apt-get install libapache2-mod-php7.0 php7.0-cli php7.0-mysql php7.0-mysqli php7.0-gd php7.0-mcrypt php7.0-json \
 php-pear snmp fping mysql-server mysql-client python-mysqldb rrdtool subversion whois mtr-tiny ipmitool \
 graphviz imagemagick apache2
+rm -rf /opt/observium
 mkdir -p /opt/observium && cd /opt
+
 
 wget http://www.observium.org/observium-community-latest.tar.gz
 tar zxvf observium-community-latest.tar.gz
