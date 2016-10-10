250e87960abfcaa7512bc9ad08df082a84600eeb
ainsey11 <robert@ainsey11.com>
2016-10-10 03:08:39 +0100

    PDU SQL Query Figured out

---
+++ php/PDU1_Usage.php
@@ -0,0 +1,22 @@
+<?PHP
+$user_name = "observium";
+$password = "observer15th3b055";
+$database = "observium";
+$server = "10.1.1.51";
+$db_handle = mysql_connect($server, $user_name, $password);
+$db_found = mysql_select_db($database, $db_handle);
+if ($db_found) {
+$SQL = "select * from `sensors-state` where sensor_id = 8";
+$result = mysql_query($SQL);
+while ( $db_field = mysql_fetch_assoc($result) ) {
+print $db_field['sensor_value'];
+}
+mysql_close($db_handle);
+}
+else {
+print "Database NOT Found ";
+mysql_close($db_handle);
+}
+
+?>
+
