b57ccdddec660a8ebb58391b1b821c7b0eae3654
ainsey11 <robert@ainsey11.com>
2016-10-14 21:22:00 +0100

    changed json to new structure for influxdb v1

--- python/Pi-GetTemps.py
+++ python/Pi-GetTemps.py
@@ -8,86 +8,91 @@ from influxdb import InfluxDBClient
 while True:
 
         sensor1 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "041501a634ff")
-        sensor1_val = sensor1.get_temperature()
+        sensor1_val = sensor1.get_temperature(W1ThermSensor.DEGREES_C)
 
         sensor2 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "041501aedaff")
-        sensor2_val = sensor2.get_temperature()
+        sensor2_val = sensor2.get_temperature(W1ThermSensor.DEGREES_C)
 
         sensor3 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "031501c516ff")
-        sensor3_val = sensor3.get_temperature()
+        sensor3_val = sensor3.get_temperature(W1ThermSensor.DEGREES_C)
 
         sensor4 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "031501c51fff")
-        sensor4_val = sensor4.get_temperature()
+        sensor4_val = sensor4.get_temperature(W1ThermSensor.DEGREES_C)
 
         sensor5 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "031501c1e1ff")
-        sensor5_val = sensor5.get_temperature()
+        sensor5_val = sensor5.get_temperature(W1ThermSensor.DEGREES_C)
 
 print("posting data")
 
+Sensor1Json = [
+{
+        "measurement": "ShedTemp",
+        "tags": {
+            "Sensor": "1",
+                },
+        "fields": {
+            "Temperature": sensor1_val
+        }
+    }
+]
+Sensor2Json = [
+{
+        "measurement": "ShedTemp",
+        "tags": {
+            "Sensor": "2",
+                },
+        "fields": {
+            "Temperature": sensor2_val
+        }
+    }
+]
 
-sensor1_json_body =[
-        {
-            "name" : "sensor1_temp_c",
-            "columns" : ["value", "sensor"],
-            "points" : [
-              [sensor1_val, "sensor01"]
-            ]
-          }
-        ]
+Sensor3Json = [
+{
+        "measurement": "ShedTemp",
+        "tags": {
+            "Sensor": "3",
+                },
+        "fields": {
+            "Temperature": sensor3_val
+        }
+    }
+]
+Sensor4Json = [
+{
+        "measurement": "ShedTemp",
+        "tags": {
+            "Sensor": "4",
+                },
+        "fields": {
+            "Temperature": sensor4_val
+        }
+    }
+]
 
-sensor2_json_body =[
-        {
-            "name" : "sensor2_temp_c",
-            "columns" : ["value", "sensor"],
-            "points" : [
-              [sensor2_val, "sensor02"]
-            ]
-          }
-        ]
+Sensor5Json = [
+{
+        "measurement": "ShedTemp",
+        "tags": {
+            "Sensor": "5",
+                },
+        "fields": {
+            "Temperature": sensor5_val
+        }
+    }
+]
 
-sensor3_json_body =[
-          {
-            "name" : "sensor3_temp_c",
-            "columns" : ["value", "sensor"],
-            "points" : [
-[sensor3_val, "sensor03"]
-            ]
-          }
-        ]
-
-sensor4_json_body =[
-          {
-            "name" : "sensor4_temp_c",
-            "columns" : ["value", "sensor"],
-            "points" : [
-              [sensor4_val, "sensor04"]
-            ]
-          }
-        ]
-
-sensor5_json_body =[
-          {
-            "name" : "sensor5_temp_c",
-            "columns" : ["value", "sensor"],
-            "points" : [
-              [sensor5_val, "sensor05"]
-            ]
-          }
-        ]
-
-
-client = InfluxDBClient('10.1.1.52', 8086, 'root', 'root', 'guckmon1')
-client.write_points(sensor1_json_body)
-client.write_points(sensor2_json_body)
-client.write_points(sensor3_json_body)
-client.write_points(sensor4_json_body)
-client.write_points(sensor5_json_body)
+client = InfluxDBClient('10.1.1.52', 8086, 'guckmon', 'guckmon', 'guckmon1')
+client.write_points(Sensor1Json)
+client.write_points(Sensor2Json)
+client.write_points(Sensor3Json)
+client.write_points(Sensor4Json)
+client.write_points(Sensor5Json)
 print(sensor1_val)
 print(sensor2_val)
 print(sensor3_val)
 print(sensor4_val)
 print(sensor5_val)
-print(sensor5_json_body)
 time.sleep(2)
 
 
