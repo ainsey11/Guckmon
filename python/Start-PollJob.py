1f6bfa73eb7f7a499e45f7ddd1b5600ebd7d37c5
ainsey11 <robert@ainsey11.com>
2016-10-15 20:48:46 +0100

    more work

--- python/Start-PollJob.py
+++ python/Start-PollJob.py
@@ -2,45 +2,62 @@ import os
 import time
 import subprocess
 from influxdb import InfluxDBClient
+from  unifi.controller import Controller
+c = Controller('10.1.1.134', 'guckmon', 'Guckmon')
 
-PDU1Usage = subprocess.check_output(['php', '/home/robert/Documents/github/Guckmon/php/PDU1_Usage.php'])
-PDU2Usage = subprocess.check_output(['php', '/home/robert/Documents/github/Guckmon/php/PDU2_Usage.php'])
+while True:
+	PDU1Usage = subprocess.check_output(['php', '/home/robert/Documents/github/Guckmon/php/PDU1_Usage.php'])
+	PDU2Usage = subprocess.check_output(['php', '/home/robert/Documents/github/Guckmon/php/PDU2_Usage.php'])
+	RouterTemp = subprocess.check_output(['php', '/home/robert/Documents/github/Guckmon/php/Router-Temp.php'])
 
-PDU1Usage = PDU1Usage.replace("\n","")
-PDU2Usage= PDU2Usage.replace("\n","")
+	PDU1Usage = PDU1Usage.replace("\n","")
+	PDU2Usage = PDU2Usage.replace("\n","")
+	RouterTemp = RouterTemp.replace("\n","")
 
 
+	PDU1Usagejson = [
+	{
+			"measurement": "PDUUsage",
+			"tags": {
+				"PDU": "1",
+					},
+			"fields": {
+				"value": PDU1Usage
+			}
+		}
+	]
 
-PDU1Usagejson = [
-{
-        "measurement": "PDUUsage",
-        "tags": {
-            "PDU": "1",
-                },
-        "fields": {
-            "value": PDU1Usage
-        }
-    }
-]
+	PDU2Usagejson = [
+	{
+			"measurement": "PDUUsage",
+			"tags": {
+				"PDU": "2",
+					},
+			"fields": {
+				"value": PDU2Usage
+			}
+		}
+	]
+	RouterTempjson = [
+	{
+			"measurement": "RouterTemperature",
+			"tags": {
+				"Router": "1",
+					},
+			"fields": {
+				"value": RouterTemp
+			}
+		}
+	]
+	client = InfluxDBClient('10.1.1.52', 8086, 'guckmon', 'guckmon', 'guckmon1')
 
-PDU2Usagejson = [
-{
-        "measurement": "PDUUsage",
-        "tags": {
-            "PDU": "2",
-                },
-        "fields": {
-            "value": PDU2Usage
-        }
-    }
-]
+	client.write_points(PDU1Usagejson)
+	client.write_points(PDU2Usagejson)
+	client.write_points(RouterTempjson)
 
-client = InfluxDBClient('10.1.1.52', 8086, 'guckmon', 'guckmon', 'guckmon1')
-
-client.write_points(PDU1Usagejson)
-client.write_points(PDU2Usagejson)
-
-print(PDU1Usage)
-print(PDU2Usage)
-print("Data Posted to InfluxDB")
+	print(PDU1Usage)
+	print(PDU2Usage)
+	print(RouterTemp)
+	print("Data Posted to InfluxDB")
+	time.sleep(30)
 
